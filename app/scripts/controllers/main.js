6cde99ac554bf2c0693f23f8641630eaa71ed166
UICandy <somesense@gmail.com>
2015-01-22 20:54:38 +0530

    JSON With Nested Array and Multiple Filtering

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,23 @@ angular.module('thoughtworksApp')
 		flightsFactory.fetch().then(function(flights) {
         $scope.flights = flights;
       });
-
-		$scope.filteredFlights = function(){
-		    var filteredFlights = [];
-		    console.log($scope.flights);
-		    angular.forEach($scope.flights, function(val){
-		      if(val !== $scope.flights){
-		        this.push(val);
-		      }
-		    },filteredFlights);
-		    return filteredFlights;
-	    };
+		$scope.filteredFlights = function(flight) {
+		    return flight.Departure.From.indexOf($scope.departsFrom) >= 0 ||
+				   flight.Arrival.At.indexOf($scope.arrivesAt) >= 0 ||
+				   flight.Departure.Date.indexOf($scope.departureDate) >= 0;
+		  };
+  })
+  .directive('autoComplete', function($timeout) {
+    return function(scope, iElement, iAttrs) {
+            iElement.autocomplete({
+                source: scope[iAttrs.uiItems],
+                select: function() {
+                    $timeout(function() {
+                      iElement.children().hide();
+                    }, 0);
+                  }
+              });
+          };
   });
+
 
--- app/scripts/filters/flightsfilter.js
+++ app/scripts/filters/flightsfilter.js
@@ -1,8 +1,47 @@
 'use strict';
 
 angular.module('thoughtworksApp')
-  .filter('flightsFilter', function () {
-    return function (input) {
-      return input;
-    };
-  });
+  .filter('filterFlights',['$filter',function ($filter) {
+  	return function (items, keyObj) {
+  	  var filterObj = {
+			data:items,
+			filteredData:[],
+			applyFilter : function(obj,key){
+				var fData = [];
+				//unfiltered data at the start
+				if(this.filteredData.length == 0)
+					this.filteredData = this.data;
+
+				if(obj){
+					var fObj = {};
+					if(!angular.isArray(obj)){
+						fObj[key] = obj;
+						fData = fData.concat($filter('filter')(this.filteredData,fObj));
+					}else if(angular.isArray(obj)){
+						console.log('bal');
+						if(obj.length > 0){	
+							for(var i=0;i<obj.length;i++){
+								if(angular.isDefined(obj[i])){
+									fObj[key] = obj[i];
+									fData = fData.concat($filter('filter')(this.filteredData,fObj));	
+								}
+							}
+							
+						}										
+					}									
+					if(fData.length > 0){
+						this.filteredData = fData;
+					}
+				}
+			}
+  				};
+
+  		if(keyObj){
+  			angular.forEach(keyObj,function(obj,key){
+  				filterObj.applyFilter(obj,key);
+  			});			
+  		}
+  		
+  		return filterObj.filteredData;
+  	}
+  }])
